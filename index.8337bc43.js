5c31414025877d525cecb7069479e2e272da249a
VladimirSt13 <99919494+VladimirSt13@users.noreply.github.com>
2022-06-25 20:06:18 +0000

    Deploying to gh-pages from @ VladimirSt13/decbase@528b5a4b1298d75a41bfe963d44b40511c87fe6a 🚀

--- index.8337bc43.js
+++ index.8337bc43.js
@@ -1,2 +1,2 @@
 $(document).ready((function(){$(".slider").slick({centerMode:!0,arrows:!0,dots:!1,slidesToShow:3,autoplay:!1,speed:1e3,autoplaySpeed:800,waitForAnimate:!1,responsive:[{breakpoint:1200,settings:{slidesToShow:2,centerMode:!1}},{breakpoint:787,settings:{slidesToShow:1,centerMode:!1}}]})}));
-//# sourceMappingURL=index.abcaf7b1.js.map
+//# sourceMappingURL=index.8337bc43.js.map
